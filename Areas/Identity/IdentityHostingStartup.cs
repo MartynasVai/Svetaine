1f870cb505427a289975c84c75748f854276103e
Martynas Vaivada <martynas.vai263@go.kauko.lt>
2021-03-07 19:16:37 +0200

    Scaffolding indentity, unhandled exception fix

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Svetaine.Areas.Identity.Data;
+using Svetaine.Data;
+
+[assembly: HostingStartup(typeof(Svetaine.Areas.Identity.IdentityHostingStartup))]
+namespace Svetaine.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<SvetaineContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("SvetaineContextConnection")));
+
+              //  services.AddDefaultIdentity<SvetaineUser>(options => options.SignIn.RequireConfirmedAccount = true)//uzkomentuoti, nes kitaip meta program.cs unhandled ecxeption
+               //     .AddEntityFrameworkStores<SvetaineContext>();
+            });
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -13,7 +13,10 @@ namespace Svetaine
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+
+                CreateHostBuilder(args).Build().Run();
+
+
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
